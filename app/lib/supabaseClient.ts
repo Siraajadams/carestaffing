08fe42160975b4eba2f955645de31853ee40a90f
Siraajadams <siraaj.adams2003@gmail.com>
2026-08-15 21:45:05 +0200

    Create supabaseClient.ts

---
+++ app/lib/supabaseClient.ts
@@ -0,0 +1,9 @@
+   [Client Component Browser]
+    ./app/employer-applicants/page.tsx [Server Component]
+  Client Component SSR:
+    ./app/employer-applicants/page.tsx [Client Component SSR]
+    ./app/employer-applicants/page.tsx [Server Component]
+https://nextjs.org/docs/messages/module-not-found
+    at <unknown> (./app/employer-applicants/page.tsx:6:1)
+    at <unknown> (https://nextjs.org/docs/messages/module-not-found)
+Error: Command "npm run build" exited with 1
